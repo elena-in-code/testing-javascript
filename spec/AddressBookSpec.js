d6f75c4407e4ba074ee214d3c88488dc6cdf6f27
Elena Moral <elenamoralpascual@gmail.com>
2018-07-23 09:31:11 +0200

    wip aync

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,4 +1,44 @@
 describe('Address Book', function() {
+    var addressBook,
+        thisContact;
+
+    beforeEach(function() {
+        addressBook = new AddressBook(),
+        thisContact = new Contact();
+    });
+
+    it('should be able to add a contact', function() {
+        addressBook.addContact(thisContact);
+
+        expect(addressBook.getContact(0)).toBe(thisContact);
+    });
+    it('should be able to remove a contact', function() {
+        addressBook.addContact(thisContact);
+        addressBook.deleteContact(0);
+
+        expect(addressBook.getContact(0)).not.toBeDefined();
+    });
+});
+
+//Async function
+describe('Async Adress Book', function() {
+    var addressBook = new AddressBook();
+
+    beforeEach(function(done) {
+        addressBook.getInitialContacts(function() {
+            done();
+        });
+    });
+
+    it('should grab initial contacts', function(done) {
+        expect(addressBook.initialComplete).toBe(true);
+        done();
+    });
+});
+
+
+/* More redundant code:
+describe('Address Book', function() {
     it('should be able to add a contact', function() {
         var addressBook = new AddressBook(),
             thisContact = new Contact();
@@ -17,3 +57,4 @@ describe('Address Book', function() {
         expect(addressBook.getContact(0)).not.toBeDefined();
     });
 });
+*/
